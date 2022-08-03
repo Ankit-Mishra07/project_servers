ea80110dc258d1a1c28ef750706e85dc4b8cee87
Ankit Mishra <90400569+Ankit-Mishra07@users.noreply.github.com>
2022-08-03 13:55:08 +0530

    server

--- src/configs/database.js
+++ src/configs/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config()
 module.exports = () => {
-    return mongoose.connect(process.env.MONGO_URL)
+    return mongoose.connect("mongodb+srv://AnkitMishra07:ankit07@cluster0.jxwud.mongodb.net/oneDoc")
 }
 
 
